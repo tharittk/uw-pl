561ca58f68a1344ac9a00a96a96acab03d993255
TharitT <tharit.tangkij@gmail.com>
2024-05-06 14:50:27 +0700

    problem b

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -5,19 +5,77 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_MyPieces = All_Pieces + 
+               [rotations([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]]), # whale
+               [[[0, 0],[-1, 0],[-2, 0], [1, 0], [2, 0]], # long 5 (only needs two)
+               [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+               rotations([[0, 0], [1, 0], [0, 1]])] # chevron
 
-  # your enhancements here
+               # your enhancements here
+  def initialize (point_array, board)
+    super(point_array, board)
+  end
+
+  def self.next_piece (board)
+    MyPiece.new(All_MyPieces.sample, board)
+  end
+  
 
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
 
+  # gets the next piece
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  def store_current
+    locations = @current_block.current_rotationx
+    displacement = @current_block.position
+    (0..(locations.size - 1)).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def initialize
+    super
+  end
 
+  def key_bindings
+    super
+    @root.bind('u', proc {
+      @board.rotate_clockwise
+      @board.rotate_clockwise
+    })
+
+  end
+
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  
 end
 
 
